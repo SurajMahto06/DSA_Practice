b58334059c2766cf01d9479dce11b9ba3a99532b
Suraj Mahto <surajcbsa06@gmail.com>
2026-08-24 22:34:54 +0530

    check palindrome

--- coding_practice/main.js
+++ coding_practice/main.js
@@ -14,3 +14,20 @@
 
 // console.log(reverseString(str));
 
+
+// ============ ====== =========== ======== Palindrom ====================
+
+// const str = "AMMN";
+
+// function checkPalindrom(str) {
+//   let reversed = "";
+
+//   for (let i = str.length-1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed === str
+// }
+
+// console.log(checkPalindrom(str));
+
+// 
