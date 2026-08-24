5d565068bc3779e7158d66814c31fc5a814bb17a
Suraj Mahto <surajcbsa06@gmail.com>
2026-08-24 22:34:11 +0530

    reverse string

---
+++ coding_practice/main.js
@@ -0,0 +1,16 @@
+// ============ ====== =========== ======== Reverse string ====================
+
+
+// const str = "AMMA";
+
+// function reverseString(str) {
+//   let reversed = "";
+
+//   for (let i = str.length-1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed
+// }
+
+// console.log(reverseString(str));
+
